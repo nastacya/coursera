cec28b58be35ff61e4a53e9b41073c57ab6b3485
Anastasija Bobrovskiha <a.bobrovskiha@inbox.lv>
2018-10-31 16:35:52 +0000

    Added assets file and created MenuComponent layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {Navbar} from "reactstrap";
 import {NavbarBrand} from "reactstrap";
+import Menu from './components/MenuComponent';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Menu/>
+            </div>
+        );
+    }
 }
 
 export default App;
