843fa46a73a0cc8880839d054d6ee27d5472aed6
Anastasija Bobrovskiha <a.bobrovskiha@inbox.lv>
2018-11-05 15:03:12 +0000

    Add new shared files, rewrite as SPA

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import {DISHES} from '../shared/dishes'
+import {COMMENTS} from "../shared/comments";
+import {LEADERS} from "../shared/leaders";
+import {PROMOTIONS} from "../shared/promotions";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
 
@@ -12,6 +16,9 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
     }
 
@@ -20,17 +27,21 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                      promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                      leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
         }
         return (
             <div>
                 <Header/>
-                    <Switch>
-                        <Route path="/home" component={HomePage} />
-                        <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> } />
-                        <Redirect to="/home" />
-                    </Switch>
+                <Switch>
+                    <Route path="/home" component={HomePage}/>
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+                    <Route exact path="/contactus" component={Contact}/>
+                    <Redirect to="/home"/>
+                </Switch>
                 <Footer/>
             </div>
 
